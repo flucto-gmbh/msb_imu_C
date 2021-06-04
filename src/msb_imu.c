f06e02165ec9930a127dce8f7c3151e31d1085d3
Aljoscha Sander <as@smoerebroed.cloud>
2021-06-04 23:20:33 +0200

    update

---
+++ src/msb_imu.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "ICM20948.h"
+
+int main (int argc, char **argv) {
+    printf("hello, motion sensor box");
+    return EXIT_SUCCESS;
+}
